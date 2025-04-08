c0849a85ac3eaccf1c08abbf845a6d6db0c1d479
VaraKind97 <valadiva@mail.ru>
2025-04-08 23:57:18 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,10 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        List<File> folderList = Arrays.asList(
-                new File("C://Games"),
-                new File("C://Games//temp"),
-                new File("C://Games//src"),
-                new File("C://Games//res"),
-                new File("C://Games//savegames"),
-                new File("C://Games//src//main"),
-                new File("C://Games//src//test"),
-                new File("C://Games//res//drawables"),
-                new File("C://Games//res//vectors"),
-                new File("C://Games//res//icons"));
-
-        List<File> fileList = Arrays.asList(
-                new File("C://Games//src//main//Main.java"),
-                new File("C://Games//src//main//Utils.java"),
-                new File("C://Games//temp//temp.txt"));
-
-        folderList.stream().forEach(folder -> {
-            if (folder.mkdir()) sb.append("Директория " + folder + " создана успешно\n");
-            else sb.append("Директория " + folder + " не создана\n");
-        });
-
-        fileList.stream().forEach(file -> {
-            try {
-                if (file.createNewFile()) sb.append("Файл " + file + " успешно создан\n");
-                else sb.append("Файлы " + file + " не созданы\n");
-            } catch (IOException ex) {
-                sb.append(ex.getMessage());
-            }
-        });
-        try (FileWriter log = new FileWriter("C://Games//temp//temp.txt", false)) {
-            log.write(sb.toString());
-            log.flush();
-        } catch (IOException ex) {
-            sb.append(ex.getMessage());
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader("C://Games//temp//temp.txt"))) {
-            String s;
-            while ((s = br.readLine()) != null) System.out.println(s);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);//Ошибка в результате деления на ноль.
+        // Исправил переменную devide в классе Calculator
+        calc.println.accept(c);
     }
 }
