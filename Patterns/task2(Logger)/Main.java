ce892447a96e6e3d2e863ac6aa9659cdc0f7a8f8
VaraKind97 <valadiva@mail.ru>
2025-04-30 00:29:41 +0300

    Add files via upload

---
+++ Patterns/task2(Logger)/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    private static int size, maxValue, treshold;
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.log("Запускаем программу");
+        logger.log("Просим пользователя ввести входные данные для списка");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер списка: " + scanner.hasNextInt());
+        System.out.println("Введите верхнюю границу для значений: " + scanner.hasNextInt());
+        logger.log("Создаём и наполняем список");
+        List<Integer> list = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(maxValue));
+        }
+        System.out.println("Вот случайный список: " + list);
+        logger.log("Просим пользователя ввести входные данные для фильтрации" );
+        System.out.println("Введите порог для фильтра: " + scanner.hasNextInt());
+        logger.log("Запускаем фильтрацию");
+        Filter filter = new Filter(treshold);
+        List<Integer> result = filter.filterOut(list);
+        logger.log("Выводим результат на экран");
+        System.out.println("Отфильтрованный список: " + result);
+        logger.log("Завершаем программу");
+    }
+}
