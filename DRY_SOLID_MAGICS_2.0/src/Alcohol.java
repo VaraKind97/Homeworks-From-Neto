a63b8e0c945b2ef1e1a755f6692e46df6462ba4a
VaraKind97 <valadiva@mail.ru>
2025-05-10 14:07:47 +0300

    Delete DRY_SOLID_MAGICS_2.0/src directory

--- DRY_SOLID_MAGICS_2.0/src/Alcohol.java
+++
@@ -1,6 +0,0 @@
-public class Alcohol extends Drink {
-
-    public Alcohol(String name, int price, String weight) {
-        super(name, price, weight);
-    }
-}
--- DRY_SOLID_MAGICS_2.0/src/Drink.java
+++
@@ -1,6 +0,0 @@
-public class Drink extends Product {
-
-    public Drink(String name, int price, String weight) {
-        super(name, price, weight);
-    }
-}
