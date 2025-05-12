c3650347169f5db6d8f235b4c7bfe75836ee588d
VaraKind97 <valadiva@mail.ru>
2025-05-12 13:02:51 +0300

    Update Alcohol.java

--- DRY_SOLID_MAGICS_2.0/src/Alcohol.java
+++ DRY_SOLID_MAGICS_2.0/src/Alcohol.java
@@ -1,18 +1,24 @@
-public class Alcohol extends Product implements Drink, StopList { 
-//Interface Segregation Principle - включил разные интерфейсы вместо одного большого, 
-//так как не каждую категорию нужно добавлять в StopList
+import java.time.LocalTime;
 
+public class Alcohol extends Product implements Drink, StopList { //Interface Segregation Principle,
+////так как не каждую категорию нужно добавлять в StopList
     public Alcohol(String name, int price, String weight) {
         super(name, price, weight);
     }
 
     @Override
     public void checkAge(String msg) {
-        System.out.format("Подтвердите возраст!");
+        System.out.println("Вы заказали алкоголь! Укажите свой возраст!");
     }
 
     @Override
     public void stop(String msg) {
-        System.out.println("С 23 часов продажа алкоголя запрещена!");
+        if (LocalTime.now().getHour() >= 23) {
+            System.out.println("Сейчас " + LocalTime.now().getHour() + ":00");
+            System.out.println("С 23 часов продажа алкоголя запрещена!");
+        } else {
+            System.out.println("Сейчас " + LocalTime.now().getHour() + ":00");
+            System.out.println("Продажа алкоголя разрешена!");
+        }
     }
 }
