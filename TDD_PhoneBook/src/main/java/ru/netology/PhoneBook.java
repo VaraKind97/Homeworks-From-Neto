9879ded184b7f77bedc98e6ddbec4d9a00426435
VaraKind97 <valadiva@mail.ru>
2025-06-05 16:33:19 +0300

    Add files via upload

---
+++ TDD_PhoneBook/src/main/java/ru/netology/PhoneBook.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import java.util.*;
+
+public class PhoneBook {
+    public static HashMap<String, Long> phoneBook = new HashMap<>();
+
+    public static int add(String name, long number) {
+        phoneBook.put(name, number);
+        return phoneBook.size();
+    }
+
+    public static String findByNumber(long number) {
+        Optional<String> result = phoneBook.entrySet().stream()
+                .filter(n -> n.getValue().equals(number))
+                .map(Map.Entry::getKey)
+                .findFirst();
+
+        return result.orElse("");
+    }
+
+    public static long findByName(String name) {
+        return phoneBook.get(name);
+    }
+
+    public static void printAllNames() {
+        Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
+        sortedMap.keySet().forEach(System.out::println);
+    }
+}
