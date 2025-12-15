a7cca0f6a173965d7d9799ae9acbec6d763161e9
VaraKind97 <valadiva@mail.ru>
2025-12-15 14:49:46 +0300

    Delete HTTP_and_Modern_Web/Handler.java

--- HTTP_and_Modern_Web/Handler.java
+++
@@ -1,9 +0,0 @@
-package ru.netology;
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-
-@FunctionalInterface
-public interface Handler {
-    public void handle(Request request, BufferedOutputStream responseStream) throws IOException;
-}
