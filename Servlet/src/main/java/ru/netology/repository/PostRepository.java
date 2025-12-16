05134f9bd364385d8400fb06d6e82ea9a3825dcb
VaraKind97 <valadiva@mail.ru>
2025-12-16 23:44:23 +0300

    Update PostRepository.java

--- Servlet/src/main/java/ru/netology/repository/PostRepository.java
+++ Servlet/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,52 +1,17 @@
 package ru.netology.repository;
-
-import ru.netology.exception.NotFoundException;
+//new class PostRepositoryStbImpladded
+//PostRepository switched to Interface
 import ru.netology.model.Post;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
-public class PostRepository {
+public interface PostRepository {
+    List<Post> all();
 
-    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    Optional<Post> getById(long id);
 
-    private final AtomicLong idPost = new AtomicLong();
+    Post save(Post post);
 
-    public List<Post> all() {
-        return new ArrayList<>(posts.values());
-    }
-
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(posts.get(id));
-    }
-
-    public Post save(Post post) {
-        Post tempPost;
-        //Если от клиента приходит пост с id=0, значит, это создание нового поста.
-        if (post.getId() == 0) {
-            idPost.incrementAndGet();
-            tempPost = new Post(idPost.get(), post.getContent());
-            posts.put(idPost.get(), tempPost);
-        }
-        //Если от клиента приходит пост с id !=0, значит, это сохранение (обновление) существующего поста.
-        else if (posts.containsKey(post.getId())) {
-            tempPost = new Post(post.getId(), post.getContent());
-            posts.replace(post.getId(), tempPost);
-        } else {
-            throw new NotFoundException("Не найден элемент с id - " + post.getId());
-        }
-        return tempPost;
-    }
-
-    public void removeById(long id) {
-        if (posts.containsKey(id)) {
-            posts.remove(id);
-        } else {
-            throw new NotFoundException("Не найден элемент с id - " + id);
-        }
-    }
+    void removeById(long id);
 }
