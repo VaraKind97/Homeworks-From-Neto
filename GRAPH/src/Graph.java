1e4781236874728a31a48ddfb71a57a2aac6decf
VaraKind97 <valadiva@mail.ru>
2026-04-25 03:33:53 +0300

    Add files via upload

---
+++ GRAPH/src/Graph.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Graph<T> {
+    private List<Vertex<T>> vertices = new ArrayList<>();
+
+    public Vertex<T> createVertex(T value) {
+        Vertex<T> v = new Vertex<>(value);
+        vertices.add(v);
+        return v;
+    }
+
+    public void createEdge(Vertex<T> a, Vertex<T> b) {
+        a.getAdjacent().add(b);
+    }
+
+    public boolean isConnected(Vertex<T> a, Vertex<T> b) {
+        return dfsFind(a, b, new HashSet<>()); // рекурсивный обход в глубину
+    }
+
+    // метод отвечает на вопрос, нашли ли мы обходом из v вершину target с учётом
+    // посещённых вершин, которые записаны в visited
+    private boolean dfsFind(Vertex<T> v, Vertex<T> target, Set<Vertex<T>> visited) {
+        // если вершина в которую зашли (v) это та которую мы искали (target), то поиск закончен
+        if (v.equals(target)) {
+            return true; // нашли
+        }
+        visited.add(v); // запоминаем вершину которую посетили
+
+        for (Vertex next : v.getAdjacent()) {
+            if (next.equals(target)) {
+                return true;
+            }
+
+            if (!visited.contains(next)) {
+                if(dfsFind(next, target, visited)) return true;
+            }
+        }
+        return false; // ничего не нашли
+    }
+
+}
